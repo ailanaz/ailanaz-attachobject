2bac49ea927a89c942a4275fd5e97716652b183b
ailanaz <117781743+ailanaz@users.noreply.github.com>
2026-05-01 12:12:34 -0500

    Rename event prefixes from 'sf' to 'ailanaz'

--- client/client.lua
+++ client/client.lua
@@ -2,33 +2,33 @@ RegisteredObjects = {}
 MyServerId = tostring(GetPlayerServerId(PlayerId()))
 
 Citizen.CreateThread(function()
-    TriggerServerEvent("sf-attachobject:ready")
-end)
+        	TriggerServerEvent("ailanaz-attachobject:ready")
+    end)
 
-RegisterNetEvent("sf-attachobject:registerObject", function (name, object)
-    if RegisteredObjects[name] ~= nil then return end
-    RegisteredObjects[name] = object
-end)
+RegisterNetEvent("ailanaz-attachobject:registerObject", function (name, object)
+        	if RegisteredObjects[name] ~= nil then return end
+        	RegisteredObjects[name] = object
+    end)
 
-RegisterNetEvent("sf-attachobject:unregisterObject", function (name)
-    if RegisteredObjects[name] == nil then return end
-    RegisteredObjects[name] = nil
-end)
+RegisterNetEvent("ailanaz-attachobject:unregisterObject", function (name)
+        	if RegisteredObjects[name] == nil then return end
+        	RegisteredObjects[name] = nil
+    end)
 
-RegisterNetEvent("sf-attachobject:registeredObjects", function(objects)
-    RegisteredObjects = objects
-end)
+RegisterNetEvent("ailanaz-attachobject:registeredObjects", function(objects)
+        	RegisteredObjects = objects
+    end)
 
 function ReqModel(modelHash)
-    local timeout = 0
-    RequestModel(modelHash)
-    while not HasModelLoaded(modelHash) and timeout < 30 do
-        timeout = timeout + 1
-        Citizen.Wait(0)
+    	local timeout = 0
+    	RequestModel(modelHash)
+    	while not HasModelLoaded(modelHash) and timeout < 30 do
+        		timeout = timeout + 1
+        		Citizen.Wait(0)
     end
 end
 
 function DeleteEntity_2(handle)
-    SetEntityAsMissionEntity(handle, true, true)
-    DeleteEntity(handle)
+    	SetEntityAsMissionEntity(handle, true, true)
+    	DeleteEntity(handle)
 end
