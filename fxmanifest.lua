6e3274d91cb2558d220659bf1ae0d470ce99781b
ailanaz <117781743+ailanaz@users.noreply.github.com>
2026-05-01 12:11:54 -0500

    Update author and version in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,31 +2,31 @@ fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
-author "mmleczek (scriptforge.gg)"
-version "1.0.1"
+author "ailanaz"
+version "1.0.0"
 
 server_exports {
-	"CreateAttachObject",
-	"RemoveAttachObject",
-	"ClearPlayerObjects",
-	"RegisterObject",
-	"UnregisterObject",
-	"GetObjectsOnPlayer",
-	"FixPlayerProps",
-	"CreateVehicleAttachObject",
-	"RemoveVehicleAttachObject",
-	"GetObjectsOnVehicle",
-	"ClearVehicleObjects",
+		"CreateAttachObject",
+		"RemoveAttachObject",
+		"ClearPlayerObjects",
+		"RegisterObject",
+		"UnregisterObject",
+		"GetObjectsOnPlayer",
+		"FixPlayerProps",
+		"CreateVehicleAttachObject",
+		"RemoveVehicleAttachObject",
+		"GetObjectsOnVehicle",
+		"ClearVehicleObjects",
 }
 
 client_scripts {
-	"client/client.lua",
-	"client/player.lua",
-	"client/vehicle.lua"
+		"client/client.lua",
+		"client/player.lua",
+		"client/vehicle.lua"
 }
 
 server_scripts {
-	"server/server.lua",
-	"server/player.lua",
-	"server/vehicle.lua"
+		"server/server.lua",
+		"server/player.lua",
+		"server/vehicle.lua"
 }
